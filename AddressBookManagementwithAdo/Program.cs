4d43e3eb37cf64c7ef73149f6772a614350eaf79
entropy-sumit <sumitr600@gmail.com>
2022-01-30 13:04:28 +0530

    [add] sumit added addressbookADO uc3 program

--- AddressBookManagementwithAdo/Program.cs
+++ AddressBookManagementwithAdo/Program.cs
@@ -7,13 +7,24 @@ namespace AddressBookManagementwithAdo
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to address book");
-           
+            AddressBookModel addressBookModel = new AddressBookModel();
+            addressBookModel.FirstName = "ShayamalDas";
+            addressBookModel.LastName = "chanchal";
+            addressBookModel.PhoneNumber = "98989789";
+            addressBookModel.Address = "Street C Model";
+            addressBookModel.City = "Gorakhpur";
+            addressBookModel.State = "Maharastra";
+            addressBookModel.ZipCode = "8787";
+            addressBookModel.Email = "hi.hello@outlook.com";
             AddressRepo repo = new AddressRepo();
             //UC1
             //repo.DataBaseConnection();
 
             //UC2
-            repo.GetAllContact();
+            //repo.GetAllContact();
+
+            //uc3
+            repo.AddDataToTable(addressBookModel);
 
             
 
--- AddressBookaADO.sql
+++ AddressBookaADO.sql
@@ -18,7 +18,22 @@ select * from AddressBookTables
 insert into AddressBookTables(FirstName,LastName,PhoneNumber,Address,City,State,ZipCode,Email)
 values('Sum','Raw','78787889','hapyind','mumbai','maharastra','256451','sumraw@gmail.com')
 
+Create procedure [dbo].[AddressBookSystemProcedure]
+(
 
+	@FirstName	varchar(150),		
+	@LastName	varchar(150),	
+	@Address	varchar(150),		
+	@City	varchar(150),	
+	@State	varchar(150),	
+	@ZipCode	varchar(6),
+    @PhoneNumber varchar(10),
+	@Email	varchar(150)	
+)	
+
+	as begin
+	Insert into AddressBookTables values(@FirstName,@LastName,@Address,@City,@State,@ZipCode,@PhoneNumber,@Email);
+	End
 
 
 
